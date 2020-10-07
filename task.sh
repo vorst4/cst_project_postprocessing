0b01b3da02840277010bf6139e09f7a93b59bade
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-10-07 12:26:37 +0200

    -

--- task.sh
+++ task.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-source /home/tue/s111167/python-env/postprocess-env/bin
+source /home/tue/s111167/python-env/postprocess-env/bin/activate
 python main.py --partition_id $partition_id \
                --n_jobs $n_jobs \
                --job_id $job_id
